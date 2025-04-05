30050aed84ee8144946ab555f6d36c6b3ce7301e
Dmitry Hrabrov <root@dexp.in>
2025-04-05 13:21:01 +0200

    Fixed: exe deploy

--- create-exe.bat
+++ create-exe.bat
@@ -4,5 +4,6 @@ python .\helpers\generate-version-file.py -v %Version% -o .\data\tmp\versionInfo
 pyinstaller --name EbookTalker --noconfirm --noupx --onedir --windowed --distpath "..\EbookTalker-Win64" --icon ".\static\favicon.ico" --splash ".\static\book.png" --add-data "C:\Python313\Lib\site-packages\transliterate;transliterate" --version-file .\data\tmp\versionInfo.rc .\desktop.py
 
 move /Y "..\EbookTalker-Win64\EbookTalker\EbookTalker.exe" "..\EbookTalker-Win64\EbookTalker.exe"
+rd /S /Q "..\EbookTalker-Win64\_internal"
 move /Y "..\EbookTalker-Win64\EbookTalker\_internal" "..\EbookTalker-Win64\_internal"
-del /Q "..\EbookTalker-Win64\EbookTalker\"
+rd /S /Q "..\EbookTalker-Win64\EbookTalker\"
