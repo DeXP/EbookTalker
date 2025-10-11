cf7314aa15be13850ca8ca511c8f3d8adfcb87e4
Dmitry Hrabrov <root@dexp.in>
2025-10-11 16:06:52 +0200

    Added: Silero-stress module to exe packer

--- create-exe.bat
+++ create-exe.bat
@@ -1,7 +1,7 @@
 set /p Version=<.\static\version.txt
 python .\helpers\generate-version-file.py -v %Version% -o .\data\tmp\versionInfo.rc
 
-pyinstaller --name EbookTalker --noconfirm --noupx --onedir --windowed --distpath "..\EbookTalker-Win64" --icon ".\static\favicon.ico" --splash ".\static\book.png" --add-data "C:\Python313\Lib\site-packages\transliterate;transliterate" --version-file .\data\tmp\versionInfo.rc .\desktop.py
+pyinstaller --name EbookTalker --noconfirm --noupx --onedir --windowed --distpath "..\EbookTalker-Win64" --icon ".\static\favicon.ico" --splash ".\static\book.png" --add-data "C:\Python313\Lib\site-packages\transliterate;transliterate" --add-data "silero_stress;silero_stress" --version-file .\data\tmp\versionInfo.rc .\desktop.py
 
 move /Y "..\EbookTalker-Win64\EbookTalker\EbookTalker.exe" "..\EbookTalker-Win64\EbookTalker.exe"
 rd /S /Q "..\EbookTalker-Win64\_internal"
